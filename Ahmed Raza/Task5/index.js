5e3e448be09d412b47abc1c0b430ff00bcb619e9
Ahmed raza <125694411+sheikhahmedraza22@users.noreply.github.com>
2023-03-31 07:21:44 -0700

    Add files via upload

---
+++ Ahmed Raza/Task5/index.js
@@ -0,0 +1,11 @@
+let isRaining = prompt("It's Raining ? true or false ? ");
+
+
+if (isRaining == "true") {
+    console.log("Bring an Umbrella");
+
+}
+else if (isRaining == "false") {
+    console.log("Leave the Umbrella at home");
+
+}
